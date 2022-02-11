6f8d28eaff071a7ecddf0bf9069d45c99cda5251
CNDRD <michal.mikulec.mm@gmail.com>
2022-02-11 20:50:00 +0100

    create this

---
+++ siegeapi/constants/app_ids.py
@@ -0,0 +1,9 @@
+SIEGE_APP_IDS = [
+    "8956241d-236d-4dbd-9e1e-bf6ed133773a",  # PC (China)
+    "e3d5ea9e-50bd-43b7-88bf-39794f4e3d40",  # PC
+    "fb4cc4c9-2063-461d-a1e8-84a7d36525fc",  # PS4
+    "6e3c99c9-6c3f-43f4-b4f6-f1a3143f2764",  # PS5
+    "e0aa997a-1626-4e5b-8215-b5f586b59fa2",  # Stadia
+    "76f580d5-7f50-47cc-bbc1-152d000bfe59",  # XBOX Series X
+    "4008612d-3baf-49e4-957a-33066726a7bc",  # XBOX One
+]
