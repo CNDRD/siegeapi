eb897bbc3adf68827277d4268e0086a8e72edc1c
CNDRD <michal.mikulec.mm@gmail.com>
2022-02-15 21:43:11 +0100

    should now work

--- siegeapi/weapons.py
+++ siegeapi/weapons.py
@@ -2,7 +2,7 @@ from .constants import WEAPONS_DICT
 
 
 class Weapon:
-    def __init__(self, data):
+    def __init__(self, data: dict):
         self.name: str = data.get("weaponName")
         self.kills: int = data.get("kills")
         self.headshots: int = data.get("headshots")
@@ -17,10 +17,28 @@ class Weapon:
         self.image_url: str = f"https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/{weapon.get('icon_url')}"
         self.type: str = weapon.get("type")
 
+    def __repr__(self) -> str:
+        return str(vars(self))
 
-class WeaponSlots:
-    ...
+
+class WeaponsGameMode:
+    def __init__(self, data: dict):
+        self.primary: list = self._get_weapons_list(data.get("teamRoles").get("all").get("weaponSlots").get("primaryWeapons").get("weaponTypes"))
+        self.secondary: list = self._get_weapons_list(data.get("teamRoles").get("all").get("weaponSlots").get("secondaryWeapons").get("weaponTypes"))
+
+    def _get_weapons_list(self, data: list[dict]) -> list[Weapon]:
+        return [Weapon(weapon) for weaponType in data for weapon in weaponType.get("weapons")]
+
+    def __repr__(self) -> str:
+        return str(vars(self))
 
 
 class Weapons:
-    ...
+    def __init__(self, data: dict):
+        self.all: WeaponsGameMode = WeaponsGameMode(data.get("platforms").get("PC").get("gameModes").get("all"))
+        self.casual: WeaponsGameMode = WeaponsGameMode(data.get("platforms").get("PC").get("gameModes").get("casual"))
+        self.ranked: WeaponsGameMode = WeaponsGameMode(data.get("platforms").get("PC").get("gameModes").get("ranked"))
+        self.unranked: WeaponsGameMode = WeaponsGameMode(data.get("platforms").get("PC").get("gameModes").get("unranked"))
+
+    def __repr__(self) -> str:
+        return str(vars(self))
