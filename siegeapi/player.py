a5336db827e31d361f9eb1676734be51afca6c5d
CNDRD <michal.mikulec.mm@gmail.com>
2022-03-17 21:25:58 +0100

    save a few lines

--- siegeapi/player.py
+++ siegeapi/player.py
@@ -11,11 +11,7 @@ from .maps import Maps
 from datetime import date
 import aiohttp
 
-PlatformURLNames = {
-    "uplay": "OSBOR_PC_LNCH_A",
-    "psn": "OSBOR_PS4_LNCH_A",
-    "xbl": "OSBOR_XBOXONE_LNCH_A"
-}
+PlatformURLNames = {"uplay": "OSBOR_PC_LNCH_A", "psn": "OSBOR_PS4_LNCH_A", "xbl": "OSBOR_XBOXONE_LNCH_A"}
 
 
 class UrlBuilder:
