fa7bedce2ea7095ecc95ae29d8400a6bb920b9e5
CNDRD <michal.mikulec.mm@gmail.com>
2022-03-26 22:12:00 +0100

    v++

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name="siegeapi",
-    version="2.0.5",
+    version="2.0.6",
     url="https://github.com/CNDRD/siege-api",
     description="Rainbow Six Siege API interface",
     author="CNDRD",
