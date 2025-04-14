b7a9cdadff10e513f8bdffec24c840b746dd1b01
CNDRD <20823071+CNDRD@users.noreply.github.com>
2025-04-14 23:23:09 +0200

    v++
    Signed-off-by: CNDRD <20823071+CNDRD@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name="siegeapi",
-    version="6.3.4",
+    version="6.3.5",
     url="https://github.com/CNDRD/siegeapi",
     description="Rainbow Six Siege API interface",
     author="CNDRD",
